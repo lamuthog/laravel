98b36710e1172ccd5f3c5dce7e858c8a6b95be87
Ubuntu <larrygify@Ubuntu-PHP.0aq0oxzurdbuvjomgkw54yjqbf.jnbx.internal.cloudapp.net>
2020-09-07 10:29:20 +0000

    Back to the Original

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Github Rocks!!
+                    Laravel
                 </div>
 
                 <div class="links">
